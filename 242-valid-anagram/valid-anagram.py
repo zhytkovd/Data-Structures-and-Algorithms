c6c246af51b39a1045ec4d76add31e12764d8d9b
Denys Zhytkov <zhytkovd@berea.edu>
2026-07-18 15:28:42 -0400

    Time: 13 ms (51.68%) | Memory: 19.6 MB (25.83%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        if len(s) != len(t):
+            return False
+        
+        freq_count1 = {}
+        freq_count2 = {}
+
+        for char1 in s:
+            if char1 in freq_count1:
+                freq_count1[char1] +=1
+            else:
+                freq_count1[char1] = 1
+        
+        for char1 in t:
+            if char1 in freq_count2:
+                freq_count2[char1] +=1
+            else:
+                freq_count2[char1] = 1
+        
+        return freq_count1.items() == freq_count2.items()
